b1532a4431aba066caab87d1ddc4a46a31277fe0
Yury <ygkolomensky@lbl.gov>
2023-01-20 11:53:33 -0800

    Week01-02

---
+++ Week01-02/lec01/hello.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+  printf("Hello, World!\n");
+  printf("Hello again, World!\n");
+  printf("Hello, Yury ! \n");
+
+  int i = 5;
+
+  float x = 3.14;
+  
+  return 11;
+}
+
